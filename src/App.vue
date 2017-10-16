e0b53523f82fcd9cd9bda9775ca282599e97ec1b
Jitse-Jan van Waterschoot <jitse-jan.van.waterschoot@itility.nl>
2017-10-16 14:24:51 +0200

    Implement Smart template with Bootstrap4, HTML4, CSS3

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
 </script>
 
 <style>
+@import url(assets/css/menu.css);
+@import url(assets/css/main.css);
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
